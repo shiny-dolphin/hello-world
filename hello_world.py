c41f9c036e72d6f0ae6db401093870cdaf6e36a8
shiny-dolphin <hafo.chen@gmail.com>
2017-11-05 15:46:42 +0900

    added my_module.py, contains two functions

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+import my_module as mm
+
 class Test():
 	def __init__(self, message):
 		print(message)
@@ -6,4 +8,6 @@ Test("Hello World")
 for number in range(1,4):
 	print(number)
 print("Hello internet")
+mm.function_one()
+mm.function_two()
 	
---
+++ my_module.py
@@ -0,0 +1,5 @@
+def function_one():
+	print("this is function one of my_module")
+	
+def function_two():
+	print("this is function two of my_module")
